c3f3aa336ba452637198dadb6c4c04d2ca4e53ff
Shaikh Imran Hossain <imran@Shaikhs-MacBook-Pro.local>
2024-03-16 14:19:18 +0600

    delete blog by using blog id

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def get_db():
 
 
 # Post a blog to datanase
-@app.post('/create-blog', status_code=status.HTTP_201_CREATED)
+@app.post('/blogs/create-blog', status_code=status.HTTP_201_CREATED)
 def create_blog(blog_req: CreateBlogSchemas, db: Session = Depends(get_db)):
     new_blog = blog_model.BlogModel(title=blog_req.title, body=blog_req.body)
     db.add(new_blog)
@@ -30,7 +30,7 @@ def create_blog(blog_req: CreateBlogSchemas, db: Session = Depends(get_db)):
 
 
 # Get all blogs
-@app.get('/blogs', status_code= status.HTTP_200_OK)
+@app.get('/blogs/all', status_code=status.HTTP_200_OK)
 def get_all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(blog_model.BlogModel).all()
     return blogs
@@ -44,3 +44,37 @@ def get_blog_by_id(blog_id, response: Response, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog {blog_id} is not available")
 
     return blog
+
+
+# Delete a blog by id
+@app.delete('/blog/delete/{blog_id}')
+def delete_a_blog(blog_id, response: Response, db: Session = Depends(get_db)):
+    is_deleted_blog = db.query(blog_model.BlogModel).filter(blog_model.BlogModel.id == blog_id).delete(
+        synchronize_session=False)
+    db.commit()
+
+    # id deleted blog 1 mean delete the blog 0 mean is not delete blog
+    if is_deleted_blog != 1:
+        print("blog is not found ")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog is not deleted due to the blog {blog_id} not found or others issues")
+
+    else:
+        return {'response': f"Deleted the blog id {blog_id}"}
+
+#
+# @app.delete("/example/return/url/{blog_id}",status_code=status.HTTP_204_NO_CONTENT)
+# def simple_return(blog_id,db: Session = Depends(get_db)):
+#     is_deleted_blog = db.query(blog_model.BlogModel).filter(blog_model.BlogModel.id == blog_id).delete(
+#         synchronize_session=False)
+#     db.commit()
+#
+#     # id deleted blog 1 mean delete the blog 0 mean is not delete blog
+#     if is_deleted_blog != 1:
+#
+#         print("blog is not found ")
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+#                             detail=f"Blog is not deleted due to the blog {blog_id} not found or others issues")
+#
+#     else:
+#         return {'body': f"simple return {blog_id}"}
